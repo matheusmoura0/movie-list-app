ab6f2cc2c1dfb4b48aa58b2e282755498245f702
Matheus Moura <matheusmoura2110@gmail.com>
2024-08-26 14:26:50 -0300

    fix: fixes sharedLink erros

--- backend/src/services/sharedLinksService.js
+++ backend/src/services/sharedLinksService.js
@@ -2,19 +2,25 @@ const SharedLink = require('../models/SharedLink');
 const Favorite = require('../models/Favorite');
 
 const createSharedLink = async (favoriteIds) => {
-    // Obtenha os favoritos únicos
     const uniqueFavoriteIds = [...new Set(favoriteIds)];
-
     const favorites = await Favorite.findAll({
         where: { id: uniqueFavoriteIds },
     });
-
     if (favorites.length !== uniqueFavoriteIds.length) {
         throw new Error('Some favorites not found');
     }
-
     const sharedLink = await SharedLink.create();
-    await sharedLink.addFavorites(favorites);
+
+    const existingFavorites = await sharedLink.getFavorites();
+    const existingFavoriteIds = new Set(existingFavorites.map(fav => fav.id));
+
+    const newFavorites = favorites.filter(fav => !existingFavoriteIds.has(fav.id));
+
+    if (newFavorites.length > 0) {
+        await sharedLink.addFavorites(newFavorites);
+    } else {
+        console.log('All selected favorites are already associated with this shared link.');
+    }
 
     return sharedLink.uuid;
 };

97a503e8d274ba3a77f130e9449b6b9b34acfd12
Matheus Moura <matheusmoura2110@gmail.com>
2024-08-26 14:29:15 -0300

    fix: comment useless code

--- backend/src/tests/integration/favoritesRoutes.test.js
+++ backend/src/tests/integration/favoritesRoutes.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const app = require('../../app');
-const SequelizeMock = require("sequelize-mock");
+//const SequelizeMock = require("sequelize-mock");
 
 jest.mock('../../models/Favorite', () => {
     const SequelizeMock = require('sequelize-mock');

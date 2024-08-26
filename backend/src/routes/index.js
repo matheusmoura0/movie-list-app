aaa4d4600b110c7d681eca0c5d7abff276fff6a6
Matheus Moura <matheusmoura2110@gmail.com>
2024-08-26 12:04:11 -0300

    feat: it starts to use the new routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const favoritesRoutes = require('./favoritesRoutes');
-//const sharedLinksRoutes = require('./sharedLinksRoutes');
+const sharedLinksRoutes = require('./sharedLinksRoutes');
+const searchRoutes = require('./searchRoutes');
 
 router.use('/favorites', favoritesRoutes);
-//router.use('/shared', sharedLinksRoutes);
+router.use('/shared', sharedLinksRoutes);
+router.use('/search', searchRoutes);
 
 module.exports = router;

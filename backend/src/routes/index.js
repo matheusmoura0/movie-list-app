1f08c388c5197b87220ce73a17413d308955704e
Matheus Moura <matheusmoura2110@gmail.com>
2024-08-26 09:41:20 -0300

    fix: it creates test file for favorites and fixes dockerfile on backend

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const favoritesRoutes = require('./favoritesRoutes');
-const sharedLinksRoutes = require('./sharedLinksRoutes');
+//const sharedLinksRoutes = require('./sharedLinksRoutes');
 
 router.use('/favorites', favoritesRoutes);
-router.use('/shared', sharedLinksRoutes);
+//router.use('/shared', sharedLinksRoutes);
 
 module.exports = router;

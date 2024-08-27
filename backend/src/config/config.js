1f634bda3fd9268b4b4f843905b2ee16f63cbd4b
Matheus Moura <matheusmoura2110@gmail.com>
2024-08-27 18:30:08 -0300

    refact: adds postregres sequelize config files

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,7 +1,31 @@
 require('dotenv').config();
 
 module.exports = {
-    PORT: process.env.PORT || 5000,
+  development: {
     API_KEY: process.env.API_KEY,
-    DATABASE_URL: process.env.DATABASE_URL || './database.sqlite',
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.POSTGRES_DIALECT || 'postgres',
+    logging: console.log,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  },
+  production: {
+    use_env_variable: 'POSTGRES_URL',
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
 };
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,10 +1,14 @@
 const { Sequelize } = require('sequelize');
-const path = require('path');
 const config = require('./config');
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: path.resolve(__dirname, '../../', config.DATABASE_URL),
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
     logging: false,
 });
 

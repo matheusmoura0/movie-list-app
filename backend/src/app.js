0f6cc2870fa0864f24b2fcec2b66f30011026142
Matheus Moura <matheusmoura2110@gmail.com>
2024-08-28 10:33:40 -0300

    fix: fixes docker-compose and separate ambients

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,12 +12,13 @@ app.use(express.json());
 
 app.use('/', routes);
 
-app.use(express.static(path.join(__dirname, '../../frontend/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../../frontend/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
-});
-
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
+  });
+}
 
 const startServer = async () => {
   try {
